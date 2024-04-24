0447e48cf80cb85a8c95295cf1cba9faf2534dab
Robin <rrlangton@gmail.com>
2024-04-24 18:24:37 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const data = require('./models/data');
+
+const app = express();
+const port = 3001;
+
+const connectionStringURI = `mongodb://127.0.0.1:27017`;
+
+const client = new MongoClient(connectionStringURI);
+
+let db;
+
+const dbName = 'usersDB';
+
+client.connect()
+  .then(() => {
+    console.log('Connected successfully to MongoDB');
+
+    db = client.db(dbName);
+
+   
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Mongo connection error: ', err.message);
+  });
+
+app.use(express.json());
+
