c43772fe0456cc765979b64c9ccae531da63d61b
Robin <rrlangton@gmail.com>
2024-04-24 18:14:41 -0500

    index.js in routes

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const user = require('./user');
-const thought = require('./thought');
-const reaction = require('./reaction');
+const User = require('./user');
+const Thought = require('./thought');
+const Reaction = require('./reaction');
 
-module.exports = { user, thought, reaction };
+module.exports = { User, Thought, Reaction };
